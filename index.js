65343db6e590f3e47f7796e7bc96b6bc97e6ad9c
Kristine Laing <kristal.tasha@gmail.com>
2022-01-03 19:59:03 +0000

    media queries completed

--- index.js
+++ index.js
@@ -2,7 +2,27 @@ const container = document.querySelector('.search');
 const btn = document.querySelector('button');
 const input = document.querySelector('input');
 
-btn.addEventListener('click', () =>{
+btn.addEventListener('click', () => {
     container.classList.toggle('active')
 
 })
+
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".menuitems");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
+const navLink = document.querySelectorAll(".navlink");
+
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
